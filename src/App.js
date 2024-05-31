f61da67c78a3f12b219c88c476a11e6742246ae4
apolline-diaz <apolline.diaz@gmail.com>
2024-05-31 10:53:06 +0200

    Add Pokedex Component out of Route in App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className='App'>
       <Header />
+      <Pokedex />
       <Routes>
         <Route path='/' element={<Pokedex />} />
         <Route path='/pokemon/:name' element={<Pokemon />} />
